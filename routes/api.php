479694c4de2c66541ffd4fb2ab4ff8660e531f6d
m.abdellatef <m.abdellatef@abgegypt.com>
2025-09-21 14:15:45 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,10 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| هنا بتسجل جميع الروتس الخاصة بالـ API بتاعك.
-| الملف بيتحمل أوتوماتيك مع RouteServiceProvider ويكون تحت middleware "api".
+|
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 Route::get('/products', [ProductsController::class, 'index']);
 Route::post('/products', [ProductsController::class, 'store']);
 Route::put('/products/{id}', [ProductsController::class, 'update']);
